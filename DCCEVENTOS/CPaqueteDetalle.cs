cacc9c84221e29fa712ede37a75999dfa8adc087
cesar <cesarmu261@gmail.com>
2023-05-22 09:56:58 -0600

    paquete detalle

---
+++ DCCEVENTOS/CPaqueteDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DCCEVENTOS
+{
+    public partial class CPaqueteDetalle : Form
+    {
+        public CPaqueteDetalle()
+        {
+            InitializeComponent();
+        }
+    }
+}
